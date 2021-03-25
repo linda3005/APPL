b2bdb5a0bce9f10f54bb521bcac8e5dbc462f639
linda3005 <56756925+linda3005@users.noreply.github.com>
2021-03-25 19:53:18 +0700

    A list of prime Done perfectly

--- W2/A List of Prime Numbers/PrimePanel.java
+++ W2/A List of Prime Numbers/PrimePanel.java
@@ -8,7 +8,8 @@ import javax.swing.*;
 public class PrimePanel extends JPanel  {  
  private JTextField number;  
  private JButton computeButton;  
- private JTextArea primeList;  
+ private JTextArea primeList; 
+ private JScrollPane scrollPane; 
  // ----------------------------------------------------------   // Sets up a panel with a heading, a labeled text field   // for the user to enter an integer, a button to trigger   // the calculation, and a text area to display the list   // of primes.  
  // ----------------------------------------------------------   
  public PrimePanel ()  
@@ -20,14 +21,16 @@ public class PrimePanel extends JPanel  {
  computeButton = new JButton ("Click to see all primes up to your number!");   
  primeList = new JTextArea (10, 30);  
  computeButton.addActionListener(new ButtonListener());  
+ scrollPane = new JScrollPane(primeList);
  // Add the components to the panel  
  add (heading);  
  add (inputLabel);  
  add (number);  
  add (computeButton);  
- add (primeList);  
+ add (scrollPane);  
  setPreferredSize (new Dimension (400, 320));  
  setBackground (Color.yellow);  
+ scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
  }  
  // *****************************************************************   // Represents a listener for the click of the button.  
  // *****************************************************************   
@@ -36,32 +39,42 @@ public class PrimePanel extends JPanel  {
  // -----------------------------------------------------------   // Generates and displays a list of primes when the  
  // button is clicked.  
  // -----------------------------------------------------------   
- public void actionPerformed (ActionEvent event)  
- {  
- String textNum = number.getText();  
- int num = Integer.parseInt (textNum);  
+ public void actionPerformed (ActionEvent event){  
+ try{
+ String textNum = number.getText(); 
+ int num = 0; 
+ num = Integer.parseInt (textNum);  
  String ans = "";  
  int count = 0;  
  if (num < 2)  
- ans = "There no primes less than " + num;   else  
- {  
- ans = " " + 2;  
- count++;  
- for (int i = 3; i <= num; i += 2)   {  
- boolean foundDivisor = false;   int j = 3;  
- while (j < i && !foundDivisor)   {  
- if (i % j == 0)   foundDivisor = true;   else  
- j++;  
+    ans = "There no primes less than " + num;   
+ else{  
+    ans = " " + 2;  
+    count++;  
+    for (int i = 3; i <= num; i += 2)   {  
+        boolean foundDivisor = false;   
+        int j = 3;  
+        while (j < i && !foundDivisor)   {  
+        if (i % j == 0)   
+        foundDivisor = true;  
+        else  
+        j++;  
  }  
  // Add i to the list if it is prime   if (j == i)  
- {  
- ans += " " + i;   count++;  
- if (count % 10 == 0)   ans += "\n";  
+ if(!foundDivisor){  
+ ans += " " + i;
+ count++;  
+ if (count % 10 == 0)   
+ ans += "\n";  
  }  
  }  
  }  
  primeList.setText (ans);  
  }
+ catch(NumberFormatException ex){
+     primeList.setText("INPUT INTEGER ONLY");
  }
  }
+ }
+}
 
