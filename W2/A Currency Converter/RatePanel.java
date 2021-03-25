936c91b831ac3e5d997f999ae4322eaf23b7ac20
linda3005 <56756925+linda3005@users.noreply.github.com>
2021-03-25 19:43:59 +0700

    A CURRENCY CONVERTER DONE

--- W2/A Currency Converter/RatePanel.java
+++ W2/A Currency Converter/RatePanel.java
@@ -7,13 +7,14 @@
 import java.awt.*;  
 import java.awt.event.*;  
 import javax.swing.*;  
+import java.text.DecimalFormat;
 public class RatePanel extends JPanel  
 { 
 private double[] rate; // exchange rates
  private String[] currencyName;  
  private JLabel result;  
- JComboBox comb = new JComboBox();
- JTextField field= new JTextField(10);
+ JComboBox comb;
+ JTextField field;
  // ------------------------------------------------------------   
  // Sets up a panel to convert cost from one of 6 currencies   
  // into U.S. Dollars. The panel contains a heading, a text
@@ -22,7 +23,8 @@ private double[] rate; // exchange rates
  // ------------------------------------------------------------   
  public RatePanel ()  
  {  
- JLabel title = new JLabel ("How much is that in dollars?");   
+ JLabel title = new JLabel ("How much is that in dollars?");
+ setPreferredSize(new Dimension(500, 250)); 
  title.setAlignmentX (Component.CENTER_ALIGNMENT);   
  title.setFont (new Font ("Helvetica", Font.BOLD, 20));  
  // Set up the arrays for the currency conversions   
@@ -32,13 +34,20 @@ private double[] rate; // exchange rates
  rate = new double [] {0.0, 1.2103, 0.7351, 0.0091, 0.6969, 0.0222, 0.0880};  
  result = new JLabel (" ------------ ");  
  //add (title);  
-  //Add Combo
-  comb.setModel(new DefaultComboBoxModel(currencyName));
-  comb.addActionListener(new ComboListener());
+ //Add Combo
+ comb = new JComboBox(currencyName);
+ comb.setAlignmentX(CENTER_ALIGNMENT);
+ field = new JTextField(15);
+ field.addActionListener(new ComboListener());
+  result = new JLabel("--Result--");
+  result.setAlignmentX(Component.CENTER_ALIGNMENT);
+  //comb.setModel(new DefaultComboBoxModel(currencyName));
+  //comb.addActionListener(new ComboListener());
+
   add(title);
   add(comb);
-  add(field);
   add(result);
+  add(field);
  }  
  // ******************************************************   
  // Represents an action listener for the combo box.   
@@ -50,10 +59,16 @@ private class ComboListener implements ActionListener {
  // displays the value in U.S. Dollars.  
  // --------------------------------------------------   
  public void actionPerformed (ActionEvent event)   {  
-     //int index = 0;
-     int index = comb.getSelectedIndex(); 
- double amount = Double.parseDouble(field.getText());
+     double amount = 0;
+     DecimalFormat decimal = new DecimalFormat("#.##");
+     try{
+         amount=Double.parseDouble(field.getText());
+     }catch(Exception ex){
+         result.setText("Input Number");
+     }
+     int ind = comb.getSelectedIndex();
  //double converted = amount*rate[index];
- result.setText (+amount+ " " + currencyName[index] + " U.S. Dollars = "+ (amount * rate[index]));   }  
+ result.setText (amount+ " " + currencyName[ind] + "=" + decimal.format(rate[ind] * amount)+"U.S Dollars");   }  
  }  
-} 
+ } 
+
